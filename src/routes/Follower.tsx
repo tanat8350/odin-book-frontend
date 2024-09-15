832818679d831184b5c7e03836f0f222c3f16b76
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-09-15 20:09:04 +0700

    add search to following and follower pages

--- src/routes/Follower.tsx
+++ src/routes/Follower.tsx
@@ -5,10 +5,13 @@ import { User } from '../configs/type';
 import { useUser } from '../configs/outletContext';
 import UserCard from '../components/UserCard';
 import checkFollowingStatus from '../utils/checkFollowingStatus';
+import { useState } from 'react';
 
 export default function Follower() {
   const { user } = useUser();
   const { id } = useParams();
+  const [search, setSearch] = useState('');
+
   const { data, isLoading, refetch } = useQuery({
     queryKey: ['followers', id],
     queryFn: async () => {
@@ -23,81 +26,101 @@ export default function Follower() {
       <h1>{data.displayName}</h1>
       <h2>@{data.username}</h2>
       <h3>Followers ({data.followedBy.length})</h3>
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       {data.followedBy.length > 0 &&
-        data.followedBy.map((follower: User) => (
-          <UserCard user={follower} key={`follower-${follower.id}`}>
-            {user?.id === id && (
-              <button
-                key={`remove-${follower.id}`}
-                onClick={async () => {
-                  const res = await api.delete(`/user/${user?.id}/follow`, {
-                    data: { userid: follower.id },
-                  });
-                  const data = await res.data;
-                  if (!data.success) {
-                    console.log('failed to remove follower');
-                  }
-                  refetch();
-                }}
-              >
-                Remove follower
-              </button>
-            )}
-
-            {user?.id !== id &&
-            user?.id !== follower.id &&
-            checkFollowingStatus(user, follower) === 'pending' ? (
-              <button
-                onClick={async () => {
-                  const res = await api.delete(`/user/${user?.id}/request`, {
-                    data: { userid: follower.id },
-                  });
-                  const data = res.data;
-                  if (!data.success) {
-                    console.log('failed to cancel request');
-                  }
-                  refetch();
-                }}
-              >
-                Cancel request
-              </button>
-            ) : user?.id !== follower.id &&
-              checkFollowingStatus(user, follower) === 'following' ? (
-              <button
-                onClick={async () => {
-                  const res = await api.delete(`/user/${follower.id}/follow`, {
-                    data: { userid: user?.id },
-                  });
-                  const data = res.data;
-                  if (!data.success) {
-                    console.log('failed to unfollow');
-                  }
-                  refetch();
-                }}
-              >
-                Unfollow
-              </button>
-            ) : (
-              user &&
-              user?.id !== follower.id && (
+        data.followedBy
+          .filter(
+            (follower: User) =>
+              follower.displayName.includes(search) ||
+              follower.username.includes(search)
+          )
+          .map((filteredFollower: User) => (
+            <UserCard
+              user={filteredFollower}
+              key={`follower-${filteredFollower.id}`}
+            >
+              {user?.id === id && (
                 <button
+                  key={`remove-${filteredFollower.id}`}
                   onClick={async () => {
-                    const res = await api.post(`/user/${follower.id}/follow`, {
-                      userid: user?.id,
+                    const res = await api.delete(`/user/${user?.id}/follow`, {
+                      data: { userid: filteredFollower.id },
                     });
                     const data = await res.data;
                     if (!data.success) {
-                      console.log('failed to follow');
+                      console.log('failed to remove follower');
                     }
                     refetch();
                   }}
                 >
-                  Follow
+                  Remove follower
                 </button>
-              )
-            )}
-          </UserCard>
-        ))}
+              )}
+
+              {user?.id !== id &&
+              user?.id !== filteredFollower.id &&
+              checkFollowingStatus(user, filteredFollower) === 'pending' ? (
+                <button
+                  onClick={async () => {
+                    const res = await api.delete(`/user/${user?.id}/request`, {
+                      data: { userid: filteredFollower.id },
+                    });
+                    const data = res.data;
+                    if (!data.success) {
+                      console.log('failed to cancel request');
+                    }
+                    refetch();
+                  }}
+                >
+                  Cancel request
+                </button>
+              ) : user?.id !== filteredFollower.id &&
+                checkFollowingStatus(user, filteredFollower) === 'following' ? (
+                <button
+                  onClick={async () => {
+                    const res = await api.delete(
+                      `/user/${filteredFollower.id}/follow`,
+                      {
+                        data: { userid: user?.id },
+                      }
+                    );
+                    const data = res.data;
+                    if (!data.success) {
+                      console.log('failed to unfollow');
+                    }
+                    refetch();
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                user &&
+                user?.id !== filteredFollower.id && (
+                  <button
+                    onClick={async () => {
+                      const res = await api.post(
+                        `/user/${filteredFollower.id}/follow`,
+                        {
+                          userid: user?.id,
+                        }
+                      );
+                      const data = await res.data;
+                      if (!data.success) {
+                        console.log('failed to follow');
+                      }
+                      refetch();
+                    }}
+                  >
+                    Follow
+                  </button>
+                )
+              )}
+            </UserCard>
+          ))}
     </>
   );
 }
--- src/routes/Following.tsx
+++ src/routes/Following.tsx
@@ -5,10 +5,13 @@ import { User } from '../configs/type';
 import { useUser } from '../configs/outletContext';
 import UserCard from '../components/UserCard';
 import checkFollowingStatus from '../utils/checkFollowingStatus';
+import { useState } from 'react';
 
 export default function Following() {
   const { user } = useUser();
   const { id } = useParams();
+  const [search, setSearch] = useState('');
+
   const { data, isLoading, refetch } = useQuery({
     queryKey: ['following', id],
     queryFn: async () => {
@@ -24,87 +27,102 @@ export default function Following() {
       <h1>{data.displayName}</h1>
       <h2>@{data.username}</h2>
       <h3>Following ({data.following.length})</h3>
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       {data.following.length > 0 &&
-        data.following.map((followingUser: User) => (
-          <UserCard user={followingUser} key={`following-${followingUser.id}`}>
-            {user?.id === id && (
-              <button
-                key={`remove-${followingUser.id}`}
-                onClick={async () => {
-                  const res = await api.delete(`/user/${user?.id}/follow`, {
-                    data: { userid: followingUser.id },
-                  });
-                  const data = await res.data;
-                  if (!data.success) {
-                    console.log('failed to remove follower');
-                  }
-                  refetch();
-                }}
-              >
-                Remove followingUser
-              </button>
-            )}
-
-            {user?.id !== id &&
-            user?.id !== followingUser.id &&
-            checkFollowingStatus(user, followingUser) === 'pending' ? (
-              <button
-                onClick={async () => {
-                  const res = await api.delete(`/user/${user?.id}/request`, {
-                    data: { userid: followingUser.id },
-                  });
-                  const data = res.data;
-                  if (!data.success) {
-                    console.log('failed to cancel request');
-                  }
-                  refetch();
-                }}
-              >
-                Cancel request
-              </button>
-            ) : user?.id !== followingUser.id &&
-              checkFollowingStatus(user, followingUser) === 'following' ? (
-              <button
-                onClick={async () => {
-                  const res = await api.delete(
-                    `/user/${followingUser.id}/follow`,
-                    {
-                      data: { userid: user?.id },
-                    }
-                  );
-                  const data = res.data;
-                  if (!data.success) {
-                    console.log('failed to unfollow');
-                  }
-                  refetch();
-                }}
-              >
-                Unfollow
-              </button>
-            ) : (
-              user &&
-              user?.id !== followingUser.id && (
+        data.following
+          .filter(
+            (following: User) =>
+              following.displayName.includes(search) ||
+              following.username.includes(search)
+          )
+          .map((filteredFollowing: User) => (
+            <UserCard
+              user={filteredFollowing}
+              key={`following-${filteredFollowing.id}`}
+            >
+              {user?.id === id && (
                 <button
+                  key={`remove-${filteredFollowing.id}`}
                   onClick={async () => {
-                    const res = await api.post(
-                      `/user/${followingUser.id}/follow`,
-                      {
-                        userid: user?.id,
-                      }
-                    );
+                    const res = await api.delete(`/user/${user?.id}/follow`, {
+                      data: { userid: filteredFollowing.id },
+                    });
                     const data = await res.data;
                     if (!data.success) {
-                      console.log('failed to follow');
+                      console.log('failed to remove follower');
                     }
                     refetch();
                   }}
                 >
-                  Follow
+                  Remove followingUser
                 </button>
-              )
-            )}
-          </UserCard>
-        ))}
+              )}
+
+              {user?.id !== id &&
+              user?.id !== filteredFollowing.id &&
+              checkFollowingStatus(user, filteredFollowing) === 'pending' ? (
+                <button
+                  onClick={async () => {
+                    const res = await api.delete(`/user/${user?.id}/request`, {
+                      data: { userid: filteredFollowing.id },
+                    });
+                    const data = res.data;
+                    if (!data.success) {
+                      console.log('failed to cancel request');
+                    }
+                    refetch();
+                  }}
+                >
+                  Cancel request
+                </button>
+              ) : user?.id !== filteredFollowing.id &&
+                checkFollowingStatus(user, filteredFollowing) ===
+                  'following' ? (
+                <button
+                  onClick={async () => {
+                    const res = await api.delete(
+                      `/user/${filteredFollowing.id}/follow`,
+                      {
+                        data: { userid: user?.id },
+                      }
+                    );
+                    const data = res.data;
+                    if (!data.success) {
+                      console.log('failed to unfollow');
+                    }
+                    refetch();
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                user &&
+                user?.id !== filteredFollowing.id && (
+                  <button
+                    onClick={async () => {
+                      const res = await api.post(
+                        `/user/${filteredFollowing.id}/follow`,
+                        {
+                          userid: user?.id,
+                        }
+                      );
+                      const data = await res.data;
+                      if (!data.success) {
+                        console.log('failed to follow');
+                      }
+                      refetch();
+                    }}
+                  >
+                    Follow
+                  </button>
+                )
+              )}
+            </UserCard>
+          ))}
     </>
   );
 }
